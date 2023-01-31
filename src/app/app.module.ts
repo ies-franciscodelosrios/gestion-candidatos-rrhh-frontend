efecf694d0a652485a81cc655f9b45d2162ffe31
“PacoRS” <paquiitoor.s@gmail.com>
2023-01-31 23:11:38 +0100

    proyecto final repo personal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,30 +4,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+
+
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { SwitchLangComponent } from './components/switch-lang/switch-lang.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SwitchLangComponent,
-    SidenavComponent
+    SidenavComponent,
+    
   ],
   imports: [ 
     BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
     AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
     HttpClientModule,
+    MatButtonModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: httpTranslateLoader,
         deps: [HttpClient]
       }
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/switch-lang/switch-lang.component.ts
+++ src/app/components/switch-lang/switch-lang.component.ts
@@ -4,7 +4,7 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-switch-lang',
   templateUrl: './switch-lang.component.html',
-  styleUrls: ['./switch-lang.component.css']
+  styleUrls: ['./switch-lang.component.scss']
 })
 export class SwitchLangComponent implements OnInit {
 
