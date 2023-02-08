5be12c6f75a7919b413b3581d5648c5085bc08d5
Juan Miguel Rivas Velasco <juanmirivas8@gmail.com>
2023-02-08 08:54:33 +0100

    servicios api insert y get

--- src/app/services/api/job.service.ts
+++ src/app/services/api/job.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Observable} from "rxjs";
+import {Job} from "../../model/job";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public insert(job: Job): Observable<boolean> {
+    return this.http.post<boolean>(`${environment.url}/Job/Insert`,job)
+  }
+
+  getById(id: number): Observable<Job> {
+    return this.http.get<Job>(`${environment.url}/Job/GetById/${id}`);
+  }
+
+  getAll(): Observable<Job[]> {
+    return this.http.get<Job[]>(`${environment.url}/Job/GetAll`);
+  }
 }
