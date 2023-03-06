0c45a45f49068f44e79a11936f43bcf3cdd00123
Juan Miguel Rivas Velasco <juanmirivas8@gmail.com>
2023-03-06 12:31:51 +0100

    version preliminar del submit creada

--- src/app/pages/home/create-job/create-job.component.ts
+++ src/app/pages/home/create-job/create-job.component.ts
@@ -5,14 +5,14 @@ import {Rol} from "../../../model/enums/Rol";
 import {SubRol} from "../../../model/enums/SubRol";
 import {Localization} from "../../../model/enums/Localization";
 import {RolStatus} from "../../../model/enums/RolStatus";
+import {Job} from "../../../model/job";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-create-job',
-  templateUrl: './create-job.component.html',
-  styleUrls: ['./create-job.component.scss']
+  selector: 'app-create-job', templateUrl: './create-job.component.html', styleUrls: ['./create-job.component.scss']
 })
 export class CreateJobComponent implements OnInit {
-  constructor(private fb: FormBuilder, private translateService:TranslateService){
+  constructor(private fb: FormBuilder, private translateService: TranslateService,private snack: MatSnackBar) {
     this.form = this.fb.group({
       project: ['', [Validators.required, Validators.maxLength(30)]],
       area: ['', [Validators.required, Validators.maxLength(20)]],
@@ -26,6 +26,7 @@ export class CreateJobComponent implements OnInit {
       status: ['', [Validators.required]],
     });
   }
+
   buttonMessageValues: string[] = ['Guardar', 'Editar'];
   buttonMessage: string = this.buttonMessageValues[0];
   rolValues: Rol[] = Object.values(Rol);
@@ -33,10 +34,40 @@ export class CreateJobComponent implements OnInit {
   subRolValues: SubRol[] = Object.values(SubRol);
   localizationValues: Localization[] = Object.values(Localization);
   statusValues: RolStatus[] = Object.values(RolStatus);
+
   ngOnInit(): void {
   }
 
   submit() {
+    const Job: Job = {
+      area: this.form.get('area')?.value,
+      candidates: [],
+      closing_date: this.form.get('closingDate')?.value,
+      creation_date: this.form.get('openingDate')?.value,
+      description: this.form.get('motive')?.value,
+      id: 0,
+      last_update: new Date(),
+      localization: this.form.get('localization')?.value,
+      proyect: this.form.get('project')?.value,
+      rol: this.form.get('rol')?.value,
+      status: this.form.get('status')?.value,
+      sub_rol: this.form.get('subRol')?.value,
+      vacancies: this.form.get('vacancies')?.value
+    }
 
+    if (this.isJobValid(Job)) {
+      console.log(Job);
+      this.snack.open('Candidato creado correctamente', 'Ok', {duration: 5000});
+    }else{
+      this.snack.open('Error al crear el candidato', 'Ok', {duration: 5000});
+    }
+  }
+
+  private isJobValid(job: Job): boolean {
+    return job.area !== undefined && job.area !== ''
+      && job.closing_date !== undefined && job.creation_date !== undefined
+      && job.localization !== undefined && job.proyect !== undefined
+      && job.proyect !== '' && job.rol !== undefined && job.status !== undefined
+      && job.sub_rol !== undefined && job.vacancies !== undefined && job.vacancies > 0;
   }
 }
